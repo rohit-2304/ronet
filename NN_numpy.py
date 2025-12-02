f32deea2e65e517209347aa633d734f8cb6ec6c0
rohit <rohitgatne2300@gmail.com>
2025-12-02 17:16:39 +0530

    changed initialization to Xavier Initialization

--- NN_numpy.py
+++ NN_numpy.py
@@ -3,8 +3,12 @@ import numpy as np
 # dense layer
 class Dense:
     def __init__(self, n_in, n_neurons, weight_regularizer_l1=0, bias_regularizer_l1=0, weight_regularizer_l2=0, bias_regularizer_l2=0):
-        self.weights = 0.01 * np.random.randn(n_in , n_neurons)   # w^T shape
-        self.baises = np.zeros((1, n_neurons))                   # b^T shape
+        upper = (6.0 / np.sqrt(n_in + n_neurons))
+        lower = -upper
+        # Xavier Initialization
+        self.weights = np.random.uniform(lower, upper, (n_in, n_neurons))    # w^T shape
+        self.biases = np.random.uniform(lower, upper, (1, n_neurons))               # b^T shape
+
         self.weight_regularizer_l1 = weight_regularizer_l1
         self.bias_regularizer_l1 = bias_regularizer_l1
         self.weight_regularizer_l2 = weight_regularizer_l2
@@ -207,6 +211,38 @@ class CrossEntropyLoss(Loss):
 
         # normalization of gradients
         self.dinputs = self.dinputs/samples
+
+class BinaryCrossEntropyLoss(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+
+        # to prevent division by zero(log(0))   
+        y_pred_clipped = np.clip(y_pred, 1e-07, 1-1e-07)    # 1-1e-07 becuase if there is some epsilon addition the log(x > 1) will be negative instead of 0
+
+        sample_losses = -(y_true*np.log(y_pred_clipped) + (1- y_true)*np.log(1- y_pred_clipped))
+        sample_losses = -np.mean(sample_losses, axis=-1)
+        return sample_losses
+    
+    def backward(self, y_pred, y_true):
+        samples = len(y_pred)       # number of samples m
+        outputs = len(y_pred[0])     # number of classes c
+        clipped_ypred = np.clip(y_pred, 1e-7,1 - 1e-7)
+        self.dinputs = -(y_true / clipped_ypred - (1- y_true)/(1- clipped_ypred))/ outputs
+        self.dinputs = self.dinputs / samples
+
+class MeanSquaredErrorLoss(Loss):
+    def forward(self, y_pred, y_true):
+        sample_losses = np.mean((y_true - y_pred)**2, axis = -1)
+        return sample_losses
+    
+    def backward(self, y_pred, y_true):
+        samples = len(y_pred)
+        outputs = len(y_pred[0])
+
+        self.dinputs = -2 * (y_true - y_pred)/ outputs
+
+        # loss over whole batch
+        self.dinputs = self.dinputs / samples
 
 # Softmax classifier - combined Softmax activation
 # and cross-entropy loss for faster backward step
@@ -493,3 +529,4 @@ class BatchNorm:
         self.dmu = np.sum(self.dx_hat * -self.std_inv, axis=0, keepdims=True) + self.dvar * np.mean(-2.0 * self.x_mu, axis=0, keepdims=True)
 
         self.dinputs = self.dx_hat * self.std_inv + self.dvar * 2 * self.x_mu/self.mean + self.dmu/self.mean
+
